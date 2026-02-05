bff3386831add235d21501801085ec1f93d420a1
cvgadingmakmurjpr <cvgadingmakmurjpr@gmail.com>
2026-02-05 11:19:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  topBtn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
